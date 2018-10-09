c5695724ad3cffeaff6916521bcca9de2c1bf752
Unbelievable <slavlen1999@gmail.com>
2018-10-09 21:38:34 +0300

    perf: improve the code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 module.exports = function solveEquation(equation) {
-var array=equation.split(' '); 
-var a=+array[0]; 
-var b=+array[4]; 
-var c=+array[8]; 
-if(array[3]=='-') b=-b; 
-if(array[7]=='-') c=-c; 
-var D = Math.sqrt(Math.pow(b,2) - 4 * a * c); 
-var x1 = (-b - D) / (2 * a); 
-var x2 = (-b + D) / (2 * a); 
-var arr = [Math.round(x1), Math.round(x2)]; 
-return arr.sort(function(a,b) { 
-return a - b; 
-});
+    const expression = equation.split(' '); 
+    let a = +expression[0]; 
+    let b = +expression[4]; 
+    let c = +expression[8];
+
+    if (expression[3] === '-') b = -b; 
+    if (expression[7] === '-') c = -c;
+
+    let D = Math.sqrt(Math.pow(b, 2) - 4 * a * c); 
+    let x1 = (-b - D) / (2 * a); 
+    let x2 = (-b + D) / (2 * a); 
+    const results = [Math.round(x1), Math.round(x2)]; 
+
+    return results.sort((a, b) => a - b);
 }
